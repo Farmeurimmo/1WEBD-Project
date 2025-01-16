5d76cd08b9fc6cb5f766eee295289567cdd2c2bc
Farmeurimmo <contact@farmeurimmo.fr>
2025-01-16 16:47:48 +0100

    fix: avoid code duplicate in omdbapi.js

--- src/omdbapi.js
+++ src/omdbapi.js
@@ -1,8 +1,6 @@
 const API_KEY = import.meta.env.VITE_API_KEY;
 
-export async function getMovies(searchTerm, page = 1) {
-    const response = await fetch('https://www.omdbapi.com/?s=' + searchTerm + '&apikey=' + API_KEY + '&type=movie&page=' + page);
-    const data = await response.json();
+function processMovies(data) {
     if (data.Response === "False") {
         return null;
     }
@@ -15,33 +13,22 @@ export async function getMovies(searchTerm, page = 1) {
         });
     }
     return data;
+}
+
+export async function getMovies(searchTerm, page = 1) {
+    const response = await fetch('https://www.omdbapi.com/?s=' + searchTerm + '&apikey=' + API_KEY + '&type=movie&page=' + page);
+    const data = await response.json();
+    return processMovies(data);
 }
 
 export async function getMovie(omdbID, plot = "full") {
     const response = await fetch('https://www.omdbapi.com/?i=' + omdbID + '&apikey=' + API_KEY + '&plot=' + plot);
     const data = await response.json();
-    if (data.Response === "False") {
-        return null;
-    }
-    if (data.Poster === "N/A") {
-        data.Poster = "/poster_empty.png";
-    }
-    return data;
+    return processMovies(data);
 }
 
 export async function getMoviesFrom2024(page = 1) {
     const response = await fetch('https://www.omdbapi.com/?s=2024&apikey=' + API_KEY + '&type=movie&y=2024&page=' + page);
     const data = await response.json();
-    if (data.Response === "False") {
-        return null;
-    }
-    if (data.Search) {
-        data.Search = data.Search.map(movie => {
-            if (movie.Poster === "N/A") {
-                movie.Poster = "/poster_empty.png";
-            }
-            return movie;
-        });
-    }
-    return data;
+    return processMovies(data);
 }
