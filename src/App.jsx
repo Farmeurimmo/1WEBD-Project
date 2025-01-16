89bef509ae9625a219829779c36aab337b8661ef
Farmeurimmo <contact@farmeurimmo.fr>
2025-01-16 13:13:27 +0100

    feat: add footer

--- src/App.jsx
+++ src/App.jsx
@@ -3,17 +3,23 @@ import Navbar from "./components/Navbar.jsx";
 import Search from "./pages/Search.jsx";
 import Home from "./pages/Home.jsx";
 import Movie from "./pages/Movie.jsx";
+import Footer from "./components/Footer.jsx";
 
 export default function App() {
     return (
-        <div className={"min-h-screen w-screen"}>
+        <div className={"w-screen"}>
             <Navbar/>
-            <Routes>
-                <Route path="/" element={<Home/>}/>
 
-                <Route path="/search" element={<Search/>}/>
-                <Route path="/movie" element={<Movie/>}/>
-            </Routes>
+            <div className={"min-h-screen"}>
+                <Routes>
+                    <Route path="/" element={<Home/>}/>
+
+                    <Route path="/search" element={<Search/>}/>
+                    <Route path="/movie" element={<Movie/>}/>
+                </Routes>
+            </div>
+
+            <Footer/>
         </div>
     )
 }
---
+++ src/components/Footer.jsx
@@ -0,0 +1,11 @@
+export default function Footer() {
+    return (
+        <footer className="footer bg-base-300 text-base-content p-6">
+            <div className="container mx-auto text-center space-y-4">
+                <p className="text-lg font-semibold">Projet réalisé par Robin MASSONNAT</p>
+                <p className="text-md">Technologies: React + Vite + TailwindCSS + DaisyUI</p>
+                <p className="text-md">API: OMDb</p>
+            </div>
+        </footer>
+    );
+}
